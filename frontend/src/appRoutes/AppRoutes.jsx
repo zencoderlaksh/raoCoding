259d6ad3f816791f5b08463d44392c5417dca71b
Shubham Jakhar <shubham15jakhar@gmail.com>
2026-05-16 18:24:25 +0530

    added layout

--- frontend/src/appRoutes/AppRoutes.jsx
+++ frontend/src/appRoutes/AppRoutes.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home"
-
+import Layout from "../layout/Layout"
+import About from "../pages/about/About"
+import Contact from "../pages/contactus/ContactUs"
 import SignUp from '../pages/auth/SignUp';
 import Login from '../pages/auth/Login';
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
     </Routes>
