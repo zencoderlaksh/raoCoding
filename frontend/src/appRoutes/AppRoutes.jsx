5284517224985a0590b522578b4316232908c1a5
kritikabhagwani <kritikabhagwani@gmail.com>
2026-05-28 17:01:02 +0530

    kritika-meetingpage

--- frontend/src/appRoutes/AppRoutes.jsx
+++ frontend/src/appRoutes/AppRoutes.jsx
@@ -9,6 +9,7 @@ import Login from '../pages/auth/Login';
 import Courses from '../pages/courses/Courses';
 import Client from '../pages/client/Client';
 import CoursePage from '../pages/courseDetails/CoursePages';
+import BookMeeting from '@/pages/bookMeeting/BookMeeting';
 const AppRoutes = () => {
   return (
     <Routes>
@@ -16,6 +17,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/meeting" element={<BookMeeting />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/client" element={<Client />} />
         <Route path="/course/:courseName" element={<CoursePage />} />
---
+++ frontend/src/pages/bookMeeting/BookMeeting.jsx
@@ -0,0 +1,11 @@
+import Cal from "@calcom/embed-react";
+
+export default function BookMeeting() {
+  return (
+    <Cal
+      calLink="kritika-bhagwani-dj37so"
+      style={{width:"100%",height:"100%"}}
+      config={{layout:"month_view"}}
+    />
+  );
+}
